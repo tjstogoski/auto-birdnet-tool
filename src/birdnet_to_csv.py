fff8baa00ffaa5f20d5a5e1f162d3c76860fed44
Thomas Stogoski <102196267+tjstogoski@users.noreply.github.com>
2025-06-26 19:08:23 -0400

    updates

--- src/birdnet_to_csv.py
+++ src/birdnet_to_csv.py
@@ -4,28 +4,38 @@ from pathlib import Path
 
 from datetime import date, time
 from birdnet import (SpeciesPredictions,
-                     predict_species_within_audio_file
+                     predict_species_within_audio_file,
+                     predict_species_at_location_and_time
 )
 import pandas as pd
 
 
 
 
-
 # Main program block
 if __name__ == "__main__":
+
+    # Generate species set to filter possible results based on lat/lon
+    species_list = predict_species_at_location_and_time(40,-105)
+    species_set = set(species_list.keys())
+
+    # Establish directory to obtain audio files from
+    # Files will be obtained from all subdirectories as well
+    base_dir = Path('.')
+
+    # Generate list of paths to all .wav audio files
+    wav_files = list(base_dir.rglob('*.wav'))
+
     # Establish empty dictionary to write results to
     result_dict = {}
 
-    # Establish directory to obtain audio files from
-    audio_data_path = '../data/raw/audio_data'
-
-    for file in os.listdir(audio_data_path):
-        # Generate file path
-        file_path = Path(f'{audio_data_path}/{os.path.basename(file)}')
+    # Loop through .wav files
+    for file_path in wav_files:
         # Run BirdNET artificial neural network
         result = SpeciesPredictions(predict_species_within_audio_file(
-            file_path, min_confidence=0.50))
+            file_path, 
+            min_confidence=0.50,
+            species_filter=species_set))
         # File name for metadata
         file_name = os.path.basename(file_path)
         # Add to dictionary
@@ -50,7 +60,7 @@ if __name__ == "__main__":
                                 'CommonName': species.split('_')[1],
                                 'Start(s)': int(time_interval[0]),
                                 'End(s)': int(time_interval[1]),
-                                'Confidence': round(confidence, 2)}
+                                'Confidence': confidence}
                 results_temp.append(species_dict)
 
     # Build DataFrame from results
@@ -67,7 +77,7 @@ if __name__ == "__main__":
                                                 'End(s)',
                                                 'Confidence'])
 
-    # Convert DataFrame to csv file
+    # Convert DataFrame to csv file and save to same folder as script
     result_species_df.to_csv(
-        Path('../data/processed/clean_bird_data.csv'),
+        Path('.clean_bird_data.csv'),
         index=False)
